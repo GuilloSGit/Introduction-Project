285e867ecf58db5f52fb549ffa4426d96cc14cdf
Guillermo Andrada <guillermoandrada@gmail.com>
2022-06-21 17:37:11 -0300

    Subiendo a Drive

--- ProyectoIntroduccion/points.js
+++ ProyectoIntroduccion/points.js
@@ -3,19 +3,22 @@ const POINTS = [
         title: 'Odea Office',
         lat: -34.599530,
         lng: -86.1560318709894,
-        image: 'https://streetviewpixels-pa.googleapis.com/v1/thumbnail?output=thumbnail&cb_client=maps_sv.tactile.gps&panoid=eckj4oxWsIMmJIyRdmW0fQ&w=588&h=290&thumb=2&yaw=312.0528&pitch=0'
+        image: 'https://streetviewpixels-pa.googleapis.com/v1/thumbnail?output=thumbnail&cb_client=maps_sv.tactile.gps&panoid=eckj4oxWsIMmJIyRdmW0fQ&w=588&h=290&thumb=2&yaw=312.0528&pitch=0',
+        description: 'Odea is a Software Company that develops software for the web and mobile devices. We are a team of developers and designers that create software that is easy to use and easy to understand.'
     },
     {
         title: 'Valle de la Luna',
         lat: -30.16380592375114,
         lng: -67.8469786709814,
-        image: 'https://lh5.googleusercontent.com/p/AF1QipNctUMlT_l4wR07omzWh1hfEa_rv-CeWkFGIhiv=s435-k-no'
+        image: 'https://lh5.googleusercontent.com/p/AF1QipNctUMlT_l4wR07omzWh1hfEa_rv-CeWkFGIhiv=s435-k-no',
+        description: 'Valle de la Luna is a mountain range in the center of the country. It is a great place to visit if you are interested in hiking and mountain biking.'
     },
     {
         title: 'Ciudad de la Plata',
         lat: -34.920528,
         lng: -57.9531702999999,
-        image: 'https://lh5.googleusercontent.com/p/AF1QipOU9F_9scnM1OGkek6E6zk662bdrvR75uWUq4dY=w203-h270-k-no'
+        image: 'https://lh5.googleusercontent.com/p/AF1QipOU9F_9scnM1OGkek6E6zk662bdrvR75uWUq4dY=w203-h270-k-no',
+        description: 'Ciudad de la Plata is a city in the center of the country. It is a great place for turists and people who want to visit the city.'
     }
 ];
 
--- ProyectoIntroduccion/pointsAdding.js
+++ ProyectoIntroduccion/pointsAdding.js
@@ -7,19 +7,35 @@ function fillPanelPuntosInteres(pointsArray) {
     for (var i = 0; i < pointsArray.length; i++) {
         var point = pointsArray[i];
         var li = document.createElement('li');
-        /* create a div with a class pointDescription */
+
+
+        /* create a div */
         var div = document.createElement('div');
-        div.className = "pointDescription";
+        div.className = "point";
         div.innerHTML = `<h3>${point.title}</h3>`;
+
+        /* create a div with a class pointDescription */
+        var divDescription = document.createElement('div');
+        divDescription.className = "pointDescription";
+        
         /* create a new div with class latAndLng */
         var divLatAndLng = document.createElement('div');
         divLatAndLng.className = "pointLatAndLng";
         divLatAndLng.innerHTML += `<p>Lat: ${point.lat}</p>`;
         divLatAndLng.innerHTML += `<p>Long: ${point.lng}</p>`;
-        div.innerHTML += `<img src="${point.image}" class="pointImage">`;
+        divLatAndLng.innerHTML += `<p>Brief: ${point.description}</p>`;
+
+        /* create a new div with class pointImage */
+        var divImage = document.createElement('div');
+        divImage.className = "pointImage";
+        divImage.innerHTML += `<img src="${point.image}" class="pointImage">`;
+
+        /* append the divs to the li */
         points.appendChild(li);
         li.appendChild(div);
-        div.appendChild(divLatAndLng);
+        div.appendChild(divDescription);
+        divDescription.appendChild(divLatAndLng);
+        divDescription.appendChild(divImage);
     }
 }
 
