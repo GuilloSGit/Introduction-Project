769ea215d839416ccfe8736aab2b5f51108c76d0
Guillermo Andrada <guillermoandrada@gmail.com>
2022-06-22 14:30:43 -0300

    ultimo

--- ProyectoIntroduccion/customMap.js
+++ ProyectoIntroduccion/customMap.js
@@ -31,20 +31,6 @@ class CustomMap {
             map: this._map,
             title: point.title,
         });
-
-        /* Abre un diálogo */
-        const infoWindow = new google.maps.InfoWindow({
-            content: `<div class="map-dialog"><h3 class="map-dialog-title">${point.title}</h3>
-            <img src="${point.image}" alt="${point.title}" class="map-point-image">
-            <p class="map-image-description">${point.description}</p>
-            </div>`,
-        });
-
-        /* Escucha el evento clic para abrir el diálogo 💬 */
-        /* Close dialog if click outside the dialog */
-        google.maps.event.addListener(marker, "click", () => {
-            infoWindow.open(this._map, marker);
-        });
     }
 
     /* Crea cada instancia de Marcador 🚩 */
@@ -57,20 +43,21 @@ class CustomMap {
         for (let point of points) {
             this._showPoint(point);
         }
+
+        /* Abre un diálogo en el mapa 🗺️ */
+        const infoWindow = new google.maps.InfoWindow({
+            content: `<div class="map-dialog"><h3 class="map-dialog-title">${point.title}</h3>
+            <img src="${point.image}" alt="${point.title}" class="map-point-image">
+            <p class="map-image-description">${point.description}</p>
+            </div>`,
+        });
+
+        google.maps.event.addListener(marker, "click", () => {
+            infoWindow.open(this._map, marker);
+        });
+
     }
-
-    // _showPolyline(points) {
-    //     const polyline = new google.maps.Polyline({
-    //         path: points,
-    //         geodesic: true,
-    //         strokeColor: "#FF0000",
-    //         strokeOpacity: 1.0,
-    //         strokeWeight: 2,
-    //     });
-
-    //     polyline.setMap(this._map);
-    // }
-
+    
     // _showPolygon(points) {
     //     const polygon = new google.maps.Polygon({
     //         paths: points,
@@ -81,5 +68,17 @@ class CustomMap {
     //         fillOpacity: 0.35,
     //     });
     //     polygon.setMap(this._map);
+    // }
+
+
+    // _showPolyline(points) {
+    //     const polyline = new google.maps.Polyline({
+    //         path: points,
+    //         geodesic: true,
+    //         strokeColor: "#FF0000",
+    //         strokeOpacity: 1.0,
+    //         strokeWeight: 2,
+    //     });
+    //     polyline.setMap(this._map);
     // }
 }
