c2e8e5444c899679b87c9cb092a77f78acccaaa2
Guillermo Andrada <guillermoandrada@gmail.com>
2022-07-15 16:31:15 -0300

    Estilos y footer

--- ProyectoIntroduccion/panel/panelStructure.js
+++ ProyectoIntroduccion/panel/panelStructure.js
@@ -2,11 +2,6 @@ class PanelStructure extends Structure {
     getHtmlContainer(id) {
         return (
             '<h5 class="panel-title"></h5>' +
-            '<span>' +
-                '<button class="btn-create" onclick="createNewPoint()">' +
-                    '<i class="fas fa-plus"></i>' +
-                '</button>' +
-            '</span>' +
             `<div id="${ id }Filter"></div>` +
             `<div id="${ id }List"><ul></ul></div>`
         );
